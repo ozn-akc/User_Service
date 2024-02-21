052ed30bba38b9525169114063234382add11cbf
Ozan Akcebe <ozan.akcebe@breuninger.de>
2024-02-21 16:03:09 +0100

    Dockerfile und Compoose

---
+++ src/main/java/de/seven/user/adapter/product/TestController.java
@@ -0,0 +1,13 @@
+package de.seven.user.adapter.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test(){
+        return "Der Service ist Live.";
+    }
+}
